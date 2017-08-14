7741b606c8a727202e926387cc22940add256eb9
nguyenviettoan95 <30643631+nguyenviettoan95@users.noreply.github.com>
2017-08-14 16:30:29 +0700

    edit file php

--- khung.php
+++ khung.php
@@ -21,6 +21,16 @@
                         <div id="menu">
                         <nav class="navbar navbar-inverse ">
                           <div class="container-fluid">
+                            <div class="navbar-header visible-xs">
+                              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                              </button>
+                              <a class="navbar-brand" href="#">Trang chủ</a>
+                            </div>
+                           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                             <ul class="nav navbar-nav">
                               <li ><a href="#">Trang chủ</a></li>
                               <li class="dropdown">
@@ -60,6 +70,7 @@
                                 </li>
                                 <li ><a href="#">Trang web tin tức</a></li>
                             </ul>
+                            </div>
                           </div>
                         </nav>
                         <!-- slide hình -->
@@ -98,14 +109,14 @@
                                     </div>
                                   </div>
                                    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
+                                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                                      <span class="glyphicon glyphicon-chevron-left"></span>
+                                      <span class="sr-only">Previous</span>
+                                    </a>
+                                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                                      <span class="glyphicon glyphicon-chevron-right"></span>
+                                      <span class="sr-only">Next</span>
+                                    </a>
   
                             </div>
                               </div>
