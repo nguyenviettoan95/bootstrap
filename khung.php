92136694a7ebac4b9764a2ef48078f30db900fab
nguyenviettoan95 <30643631+nguyenviettoan95@users.noreply.github.com>
2017-08-14 16:06:30 +0700

    first commit

---
+++ khung.php
@@ -0,0 +1,179 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Bootstrap</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" ><link>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css" ><link>
+    <link rel="stylesheet" type="text/css" href="khung.css"></link>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    </head>
+    <body class="container">
+           <div class="row">
+                <div class="col-md-12">
+                    <div id="head">
+                        <img alt="Fjords" src="logo81.png"><br><br>
+                        <div id="menu">
+                        <nav class="navbar navbar-inverse ">
+                          <div class="container-fluid">
+                            <ul class="nav navbar-nav">
+                              <li ><a href="#">Trang chủ</a></li>
+                              <li class="dropdown">
+                                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Tuyển sinh
+                                <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                  <li><a href="#">Sau đại học</a></li>
+                                  <li><a href="#">Đại học và Cao đẳng</a></li>
+                                </ul>
+                              </li>
+                              <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Đào Tạo<span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                  <li><a href="#">Sau đại học</a></li>
+                                  <li><a href="#">Đại học và Cao đẳng</a></li>
+                                </ul>
+                                </li>
+                              <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Nghiên cứu<span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                  <li><a href="#">Chuyên san KHTN thuộc tạp chí PT KH&CN</a></li>
+                                </ul>
+                                </li>
+                                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Đơn vị<span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                  <li><a href="#">Các phòng chức năng</a></li>
+                                  <li><a href="#">Các khoa</a></li>
+                                  <li><a href="#">Các phòng thí nghiệm</a></li>
+                                  <li><a href="#">Các trung tâm</a></li>
+                                </ul>
+                                </li>
+                                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Đảng - Đoàn thể<span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                  <li><a href="#">Đảng bộ</a></li>
+                                  <li><a href="#">Công đoàn</a></li>
+                                  <li><a href="#">Đoàn thanh niên-Hội sinh viên</a></li>
+                                  <li><a href="#">Hội cựu chiến binh</a></li>
+                                </ul>
+                                </li>
+                                <li ><a href="#">Trang web tin tức</a></li>
+                            </ul>
+                          </div>
+                        </nav>
+                        <!-- slide hình -->
+                        <div class="clearfix"></div>
+                        <div class="col-md-12">
+                        <div id="myCarousel" class="carousel" data-ride="carousel">
+                            <!-- Indicators -->
+                            <ol class="carousel-indicators">
+                              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                              <li data-target="#myCarousel" data-slide-to="1"></li>
+                              <li data-target="#myCarousel" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+
+                                  <div class="item active">
+                                    <img src="la.jpg" alt="Los Angeles" style="width:100%;">
+                                    <div class="carousel-caption">
+                                      <h3>Los Angeles</h3>
+                                      <p>LA is always so much fun!</p>
+                                    </div>
+                                  </div>
+                                                              
+                                  <div class="item">
+                                    <img src="chicago.jpg" alt="Chicago" style="width:100%;">
+                                    <div class="carousel-caption">
+                                      <h3>Chicago</h3>
+                                      <p>Thank you, Chicago!</p>
+                                    </div>
+                                  </div>
+                                
+                                  <div class="item">
+                                    <img src="ny.jpg" alt="New York" style="width:100%;">
+                                    <div class="carousel-caption">
+                                      <h3>New York</h3>
+                                      <p>We love the Big Apple!</p>
+                                    </div>
+                                  </div>
+                                   <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  
+                            </div>
+                              </div>
+                </div>
+                        <!-- kết thúc phần slide hình -->
+                        </div>
+                    </div>
+                </div>
+                 <div class="clearfix"></div><br>
+                <div class="col-md-12">
+                    <div class="tieude1">
+                            TIN TỨC - SỰ KIỆN
+                    </div>
+                </div>
+                 <div class="clearfix"></div><br>
+                <div class="col-xs-12 col-md-4">
+                        <div class="tieude">
+                            <P>THÔNG TIN DÀNH CHO CB-CNV-NLĐ</P>
+                        </div>
+                    <div class="panel panel-default panel-body">
+                        
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-4">
+                    <div class="tieude">
+                                <P>THÔNG TIN DÀNH CHO NGƯỜI HỌC</P>
+                            </div>
+                    <div class="panel panel-default panel-body">
+                        <a href="http://www.hcmus.edu.vn/component/content/article/109-ph%C3%B2ng-ctsv/thong-tin-danh-cho-sinh-vien/115-thong-bao-nhan-thuong-hk2-16-17-doi-voi-sinh-vien-dat-thanh-tich-cao-ky-xet-tuyen-nam-2016.html?Itemid=437">Thông báo nhận thưởng HK2/16-17 đối với sinh viên đạt thành tích cao kỳ xét tuyển năm 2016</a><br>
+                        <a href="http://www.hcmus.edu.vn/component/content/article/85-th%C3%B4ng-b%C3%A1o-d%C3%A0nh-cho-sinh-vi%C3%AAn/111-thong-bao-vv-xet-tang-giai-thuong-le-van-thoi-danh-cho-de-tai-tot-nghiep-xuat-sac-nam-2017.html?Itemid=437">Thông báo vv xét tặng giải thưởng Lê Văn Thới dành cho đề tài tốt nghiệp xuất sắc năm 2017</a><br>
+                        
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-4">
+                    <div class="tieude">
+                            <P>TIN HOẠT ĐỘNG</P>
+                        </div>
+                    <div class="panel panel-default panel-body">
+                        
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+                <div class=" col-md-12">
+                    <div class="panel panel-default panel-body">
+                    E
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+                <div class="col-xs-12">
+                <div class="col-xs-12 col-sm-6">
+                    <div class="panel panel-default panel-body">
+                    <img  class="img-responsive" alt="" src="frink_roatan_2015_460.jpg" style="width:500px;height:300px;">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6">
+                    <div class="panel panel-default panel-body">
+                    Even though real hovering isn't possible on most touchscreens, most mobile browsers emulate hovering support and make :hover "sticky". In other words, :hover styles start applying after tapping an element and only stop applying after the user taps some other element. This can cause Bootstrap's :hover states to become undesirably "stuck" on such browsers. Some mobile browsers also make :focus similarly sticky. There is currently no simple workaround for these issues other than removing such styles entirely.
+                    </div>
+                </div>
+                </div>
+                <div class="clearfix"></div>
+                <div class="col-md-12">
+                    <div class="panel panel-default panel-body">
+                    H
+                    </div>
+                </div>
+           </div>
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+            <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
